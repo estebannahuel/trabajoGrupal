91196279abada96f26a8f8cca1bd58f49269fc9d
estebannahuel <estebanaugieri44@gmail.com>
2023-06-01 11:07:14 -0300

    rama develop

---
+++ tareas/index.js
@@ -0,0 +1,15 @@
+//const {leerJSON, escribirJSON} = require("../data")
+//const Tarea = require("./Tareas")
+
+
+module.exports = {
+    //tareas: leerJSON(),
+    //listar : require("./listar"), 
+    //agregar : require("./agregar"), 
+    //filtrar: require("./filtrar"),
+    //editar : require("./editar"), 
+    //eliminar : require("./eliminar")
+        
+
+    
+}
