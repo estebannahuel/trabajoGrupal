d3863eaf5f122225e7cc4cac9f3e5acb4357a352
ClaraZalazar <zalazarmariaclara@gmail.com>
2023-06-01 20:09:41 -0300

    subo el require de la funcion agregar que està en el archivo homònimo

--- tareas/index.js
+++ tareas/index.js
@@ -1,11 +1,11 @@
 const {leerJSON, escribirJSON} = require("../data")
-//const Tarea = require("./Tareas")
+const Tarea = require("./Tareas")
 
 
 module.exports = {
     tareas: leerJSON(),
     listar : require("./listar"), 
-    //agregar : require("./agregar"), 
+    agregar : require("./agregar"), 
     //filtrar: require("./filtrar"),
     //editar : require("./editar"), 
     //eliminar : require("./eliminar")
